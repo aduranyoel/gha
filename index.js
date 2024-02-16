1c5ad9b2ebb4c0bb233d7200144f543aa4848387
Yoel Duran <yduran@thaloz.com>
2024-02-16 18:33:30 -0300

    test updates

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.end('It works!\n');
+    res.end('It Works!\n');
 })
 
 app.listen(3000, () => {
-    console.log('app running on port 3000');
+    console.log('app running on port: 3000');
 });
