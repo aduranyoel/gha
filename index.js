9bfec0f36afc26c50519b0e69b4fe1ebf21ad4dd
Yoel Duran <yduran@thaloz.com>
2024-02-17 02:22:57 -0300

    test3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.end('It Works!!!\n1');
+    res.end('It Works!!!\n2');
 })
 
 app.listen(80, () => {
